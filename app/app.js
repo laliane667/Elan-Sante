dd6331c5fcec82aa9ac309d39ea9400bd3a1dc2d
Elian Lamouroux <elian.lamouroux@gmail.com>
2023-09-13 10:07:27 +0200

    Maj

--- app/app.js
+++ app/app.js
@@ -1,16 +1,3 @@
-const loader = document.querySelector('.loader');
-
-window.addEventListener('load', () =>{
-    /* $("#loader-logo").css({'opacity' : '1.'}); */
-    loader.classList.add('shade-out');
-    loader.classList.add('index-out');
-    $("body").css({'overflow' : 'visible'});
-
-    /* if ($(window).width() < 800) {
-        $('.main__downButton').css("opacity" ,'1');
-    } */
-})
-
 const menu = document.querySelector('#mobile-menu');
 const menuLink = document.querySelector('.navbar__menu');
 const catalogContainer = document.querySelector('.mtCatalog__container');
@@ -44,36 +31,6 @@ function updateWidthResponse(){
             addOrRemoveLogo = false;
             $(".navbar__menu").prepend(li);
         }
-    }
-
-    /* if($(window).width() > 1200){
-        if($(window).height() < 777){
-            //section container bottom 2
-            $(".sectionContainer").css({"bottom" : "2%"});
-            $("#presentation_sentance").css({"bottom" : "27%"});
-            $("#presentation_sentance").css({"padding" : "0 2%"});
-        }else{
-            $(".sectionContainer").css({"bottom" : "10%"});
-            $("#presentation_sentance").css({"bottom" : "35%"});
-            $("#presentation_sentance").css({"padding" : "0 10%"});
-        }
-    } */
-    
+    }    
 }
 
-/* $('.main__downButton').click(function(){
-    $("html, body").animate({ scrollTop: "750" }, 800);
-});
- */
-/* window.addEventListener("scroll", function(event) {
-    updateVisuals();
-});
-
-function updateVisuals(){
-    var scroll_y = this.scrollY;
-    if(scroll_y > 200){
-        $('.main__downButton').css("opacity" ,'0');
-    }else if ($(window).width() < 1060) {
-        $('.main__downButton').css("opacity" ,'1');
-    }
-} */
